d03e039fa6da506b81218d3ab346c50ab41f82c6
EfrainSaldana <efrain.p.saldana@gmail.com>
2018-11-06 06:59:57 +0000

    Gameover works, gamewin could use improvements. Working on ordering letters to help out the player

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import random
 hangman_parts = [ "head", "left arm", "torso", "right arm", "left leg", "right leg" ]
 num_wrong_guesses_allowed = len(hangman_parts)
 words = [
@@ -45,3 +46,31 @@ def draw_hangman(num_wrong_guesses):
         output = output + "\n |"
     output = output + "____\n\n"
     print(output)
+
+playing = True
+
+
+word = random.choice(words)
+done = False
+wrong_guesses = []
+right_guesses = []
+while (not done) :
+    gameover = ((len(wrong_guesses)+2) > num_wrong_guesses_allowed)
+    gamewin = (len(right_guesses) > (len(word)))
+    guess = input("What is your letter? ").lower()
+    if guess in word:
+        right_guesses.append(guess)
+        draw_hangman(len(wrong_guesses))
+        print("yay")
+        if(gamewin):
+            print ("You won!")
+            continue
+        continue
+    else:
+            wrong_guesses.append(guess)
+            draw_hangman(len(wrong_guesses))
+            print (wrong_guesses)
+            if(gameover):
+                done = True
+                print("Game over, sorry")
+            continue
